41569bfa3c83f81e15551cf591ef5eadac71e6af
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-11 21:07:13 -0500

    Webpage now builds out the time blocks on load, and colors them according to the time of day

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,42 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // This whole section builds the time blocks on the planner
+  for (var i =9; i < 18; i++) {
+    var imperial = i;
+    //Changes the displayed hour to standard time vs military time
+    if (imperial > 12 ) {
+      imperial -= 12;
+    }
+    var nextID = `hour-${imperial}`;
+    //Changes AM to PM after 11
+    let pastNoon = "AM";
+    if (i > 11) {
+      pastNoon = "PM"
+    }
+
+    // Adds the actual time block to the screen
+    $("#time-blocks").append(
+    `<div id=${nextID} class="row time-block">
+      <div class="col-2 col-md-1 hour text-center py-3">${imperial+pastNoon}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+    </div>`)
+
+    // Changes the color of the time block depending on the time of day
+    if (i < dayjs().hour()) {
+      $(`#${nextID}`).addClass("past");
+    } else if (i = dayjs().hour()) {
+      $(`#${nextID}`).addClass("present");
+    } else {
+      $(`#${nextID}`).addClass("future");
+    }
+
+
+
+  }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,13 +56,14 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
   // TODO: Add code to display the current date in the header of the page.
   
-
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMM D"));
   // This is missing the ordinal suffix because I couldn't figure out how to use dayjs
   // advancedFormat
-
+  console.log(today.hour());
 
 });
